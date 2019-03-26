31147c3b9fd5fbfdbbc1347ab1db93d9095b9369
Keith Damiani <keith@tighten.co>
2019-03-26 15:06:05 -0400

    Add Tailwind, update base component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,15 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app" class="flex items-center justify-center h-screen w-screen bg-grey-lighter">
+        <HelloWorld message="Hello" />
+    </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+    components: {
+        HelloWorld,
+    },
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+import './main.css';
+
 import Vue from 'vue'
 import App from './App.vue'
 
